432c9ce45e5a1f234709c4ca75eb87fb0b59785f
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-05 18:28:39 +0100

    use the patched version; transfer disable flag

--- stage3/01-csdr/00-run-chroot.sh
+++ stage3/01-csdr/00-run-chroot.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 
 pushd /tmp
-git clone https://github.com/jketterl/csdr.git -b 48khz_filter
+git clone https://github.com/jketterl/csdr.git
 pushd csdr
-make PARAMS_SIMD="-mfloat-abi=hard -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mvectorize-with-neon-quad -funsafe-math-optimizations -Wformat=0 -DNEON_OPTS"
+make PARAMS_SIMD="-mfloat-abi=hard -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mvectorize-with-neon-quad -funsafe-math-optimizations -Wformat=0 -DNEON_OPTS -DCSDR_DISABLE_FFTW_MEASURE"
 sudo make install
 popd
 rm -rf csdr
