15271370c413e9ebf714a9b8c5ad5e148e76e8a1
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-19 22:18:29 +0100

    install the other configs, too

--- stage3/20-openwebrx/00-run.sh
+++ stage3/20-openwebrx/00-run.sh
@@ -13,5 +13,7 @@ EOF
 mkdir "${ROOTFS_DIR}/tmp/openwebrx"
 echo "tmpfs /tmp/openwebrx tmpfs defaults,noatime,nosuid,size=64m 0 0" >> "${ROOTFS_DIR}/etc/fstab"
 
-mv "${ROOTFS_DIR}/opt/openwebrx/config_webrx.py" "${ROOTFS_DIR}/opt/openwebrx/config_webrx.py.orig"
-sed 's/temporary_directory = "\/tmp"/temporary_directory = "\/tmp\/openwebrx"/' < "${ROOTFS_DIR}/opt/openwebrx/config_webrx.py.orig" > "${ROOTFS_DIR}/opt/openwebrx/config_webrx.py"
+mkdir -p ${ROOTFS_DIR}/etc/openwebrx
+sed 's/temporary_directory = "\/tmp"/temporary_directory = "\/tmp\/openwebrx"/' < "${ROOTFS_DIR}/opt/openwebrx/config_webrx.py" > "${ROOTFS_DIR}/etc/openwebrx/config_webrx.py"
+install -m 644 "${ROOTFS_DIR}/opt/openwebrx/bookmarks.json" "${ROOTFS_DIR}/etc/openwebrx/bookmarks.json"
+install -m 644 "${ROOTFS_DIR}/opt/openwebrx/bands.json" "${ROOTFS_DIR}/etc/openwebrx/bands.json"
