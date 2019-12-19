bc02766e51b6a4482e81771f50e429715577a70e
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-19 22:17:20 +0100

    cmake cpu detection should now work without overrides

--- stage3/01-csdr/00-run-chroot.sh
+++ stage3/01-csdr/00-run-chroot.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 pushd /tmp
 git clone https://github.com/jketterl/csdr.git
 pushd csdr
-make PARAMS_SIMD="-mfloat-abi=hard -march=armv7-a -mtune=cortex-a8 -mfpu=neon -mvectorize-with-neon-quad -funsafe-math-optimizations -Wformat=0 -DNEON_OPTS -DCSDR_DISABLE_FFTW_MEASURE"
+make
 sudo make install
 popd
 rm -rf csdr
