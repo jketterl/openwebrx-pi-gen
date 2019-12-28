366fe01def67b89d4c3eeeddff2e71873d3f6e5e
Jakob Ketterl <jakob.ketterl@gmx.de>
2019-12-28 23:30:29 +0100

    fix git command

--- stage3/09-owrx-connector/01-run-chroot.sh
+++ stage3/09-owrx-connector/01-run-chroot.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 pushd /tmp
-git --depth 1 clone https://github.com/jketterl/owrx_connector.git
+git clone --depth 1 https://github.com/jketterl/owrx_connector.git
 pushd owrx_connector
 mkdir build
 pushd build
