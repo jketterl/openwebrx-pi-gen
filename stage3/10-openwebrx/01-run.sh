69717b0851bee0420135115bc2df17223b2a909d
Jakob Ketterl <jakob.ketterl@gmx.de>
2020-02-20 19:34:02 +0100

    implement the tmpfs again

---
+++ stage3/10-openwebrx/01-run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefile
+
+# move openwebrx temporary file off into tmpfs (ramdisk)
+mkdir "${ROOTFS_DIR}/tmp/openwebrx"
+echo "tmpfs /tmp/openwebrx tmpfs defaults,noatime,nosuid,size=64m 0 0" >> "${ROOTFS_DIR}/etc/fstab"
+
+cp "${ROOTFS_DIR}/etc/openwebrx/config_webrx.py" "${ROOTFS_DIR}/tmp/config_webrx.py"
+sed 's/temporary_directory = "\/tmp"/temporary_directory = "\/tmp\/openwebrx"/' < "${ROOTFS_DIR}/tmp/config_webrx.py" > "${ROOTFS_DIR}/etc/openwebrx/config_webrx.py"
+rm "${ROOTFS_DIR}/tmp/config_webrx.py"
