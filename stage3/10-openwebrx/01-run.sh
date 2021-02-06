50f8fee928df716b0608ed507ab0db25a953ece8
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-02-06 22:47:13 +0100

    use overrides to update the temporary directory

--- stage3/10-openwebrx/01-run.sh
+++ stage3/10-openwebrx/01-run.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 mkdir "${ROOTFS_DIR}/tmp/openwebrx"
 echo "tmpfs /tmp/openwebrx tmpfs defaults,noatime,nosuid,size=64m 0 0" >> "${ROOTFS_DIR}/etc/fstab"
 
-cp "${ROOTFS_DIR}/etc/openwebrx/config_webrx.py" "${ROOTFS_DIR}/tmp/config_webrx.py"
-sed 's/temporary_directory = "\/tmp"/temporary_directory = "\/tmp\/openwebrx"/' < "${ROOTFS_DIR}/tmp/config_webrx.py" > "${ROOTFS_DIR}/etc/openwebrx/config_webrx.py"
-rm "${ROOTFS_DIR}/tmp/config_webrx.py"
+cat << EOF > "${ROOTFS_DIR}/etc/openwebrx/openwebrx.conf.d/20-temporary-directory.conf"
+[core]
+temporary_directory = /tmp/openwebrx
+EOF
